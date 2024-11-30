df2071a28ecce720a278c33a6c3611a83f1b93da
lukaszsawina <lukisaw01@gmail.com>
2024-11-30 15:38:06 +0100

    Quality Option select added

---
+++ Viewer/Model/Quality/IQualityOption.cs
@@ -0,0 +1,11 @@
+﻿namespace Viewer.Model.Quality;
+
+public interface IQualityOption
+{
+    public int MaxImageSize { get; }
+    public int WindowRadius { get; }
+    public int WindowStep { get; }
+    public int NumSamples { get; }
+    public int NumIterations { get; }
+    public int CheckNumImages { get; }
+}
---
+++ Viewer/Model/Quality/MediumQualityModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Viewer.Model.Quality;
+
+public class MediumQualityModel : IQualityOption
+{
+    public int MaxImageSize => 1600;
+    public int WindowRadius => 4;
+    public int WindowStep => 2;
+    public int NumSamples => 10;
+    public int NumIterations => 5;
+    public int CheckNumImages => 33;
+}
